acbe4ed4a32969f9f67c46d7aa967e657ce49c78
Luke Elrod <luke.elrod.career@gmail.com>
2024-06-17 21:17:44 -0700

    Slight delay re added

--- Core.lua
+++ Core.lua
@@ -23,48 +23,51 @@ AutoConfirmationSettings = {
 }
 
 local function PopupHook(self)
+    --debug
+    --print(self.which)
+
     if (self.which == "ABANDON_QUEST" or self.which == "ABANDON_QUEST_WITH_ITEMS") and AutoConfirmationSettings.abandonQuest then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "CONFIRM_LOOT_ROLL" and AutoConfirmationSettings.confirmLootRoll then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "DELETE_GOOD_ITEM" and AutoConfirmationSettings.autoDeleteItem then
         self.editBox:SetText("delete")
     elseif self.which == "CONFIRM_REMOVE_GLYPH" and AutoConfirmationSettings.removeGlyph then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "CONFIRM_GLYPH_PLACEMENT" and AutoConfirmationSettings.confirmGlyphPlacement then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "CONFIRM_UPGRADE_ITEM" and AutoConfirmationSettings.confirmItemReforge then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "CONFIRM_LOOT_DISTRIBUTION" and AutoConfirmationSettings.confirmLootDistribution then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "CONFIRM_BATTLEFIELD_ENTRY" and AutoConfirmationSettings.confirmBattlefieldEntry then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "DELETE_MAIL" and AutoConfirmationSettings.deleteMailAutomatically then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "DEATH" and AutoConfirmationSettings.releaseSpirit then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "RESURRECT" and AutoConfirmationSettings.acceptResurrect then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "acceptTrade" and AutoConfirmationSettings.acceptTrade then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "LOOT_BIND" and AutoConfirmationSettings.confirmLootBind then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "EQUIP_BIND" and AutoConfirmationSettings.confirmEquipBind then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "USE_BIND" and AutoConfirmationSettings.confirmUseBind then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "QUEST_ACCEPT" and AutoConfirmationSettings.acceptSharedQuest then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "BIND_ENCHANT" and AutoConfirmationSettings.confirmEnchantBind then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "REPLACE_ENCHANT" and AutoConfirmationSettings.confirmReplaceEnchant then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "CONFIRM_LEAVE_INSTANCE_PARTY" and AutoConfirmationSettings.confirmLeaveInstance then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "CONFIRM_LEAVE_BATTLEFIELD" and AutoConfirmationSettings.confirmLeaveBattlefield then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     elseif self.which == "CONFIRM_SURRENDER_ARENA" and AutoConfirmationSettings.confirmSurrenderArena then
-        self.button1:Click()
+        C_Timer.After(0.05, function() self.button1:Click() end)
     end
 end
 
